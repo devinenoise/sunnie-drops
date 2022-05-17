7d5414a090d57dae44a5651e4723eb77336ef02b
Kyle Devine <devinenoise@gmail.com>
2022-05-17 03:40:22 -0700

    input quantity

--- assets/theme.js
+++ assets/theme.js
@@ -8485,3 +8485,27 @@ var afterResize = (function () {
     t[uniqueId] = setTimeout(callback, ms);
   };
 })();
+
+/* Quantity buttons for inputs */
+class QuantityInput extends HTMLElement {
+  constructor() {
+    super();
+    this.input = this.querySelector('input');
+    this.changeEvent = new Event('change', { bubbles: true });
+
+    this.querySelectorAll('button').forEach(button =>
+      button.addEventListener('click', this.onButtonClick.bind(this))
+    );
+  }
+
+  onButtonClick(event) {
+    event.preventDefault();
+    const previousValue = this.input.value;
+
+    event.target.name === 'plus' ? this.input.stepUp() : this.input.stepDown();
+    if (previousValue !== this.input.value)
+      this.input.dispatchEvent(this.changeEvent);
+  }
+}
+
+customElements.define('quantity-input', QuantityInput);
