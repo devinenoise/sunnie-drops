d17e8ae6db6c223e9377f267e16fb9cd22c5a554
Kyle Devine <devinenoise@gmail.com>
2022-05-24 19:41:46 -0700

    separate script for quantity input js

--- assets/theme.js
+++ assets/theme.js
@@ -8486,27 +8486,4 @@ var afterResize = (function () {
   };
 })();
 
-/* Quantity buttons for inputs */
-class QuantityInput extends HTMLElement {
-  constructor() {
-    super();
-    this.input = this.querySelector('input');
-    this.changeEvent = new Event('change', { bubbles: true });
 
-    this.querySelectorAll('button').forEach(button =>
-      button.addEventListener('click', this.onButtonClick.bind(this))
-    );
-  }
-
-  onButtonClick(event) {
-    event.preventDefault();
-    const previousValue = this.input.value;
-    console.log('clicked');
-
-    event.target.name === 'plus' ? this.input.stepUp() : this.input.stepDown();
-    if (previousValue !== this.input.value)
-      this.input.dispatchEvent(this.changeEvent);
-  }
-}
-
-customElements.define('quantity-input', QuantityInput);
